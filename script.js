50e9ccfff9d16923740d8958759f8ac53d863a07
Cristian Gabriel Iuga <gabrieliuga95@gmail.com>
2024-10-30 16:00:59 +0200

    site 4

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-function sendEmail()
-{
+function sendMail(){
     let parms = {
-        name: document.getElementById("email");
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        subject: document.getElementById("subject").value,
+        message: document.getElementById("message").value,
     }
+
+    emailjs.sendMail("service_xjj9p4s","template_f3kjqor",parms).then(alert("Email sent!"))
 }
